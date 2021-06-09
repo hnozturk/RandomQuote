9605e0c65a392fd4b7e726b80ad14149d769921b
Nazli Ayaz <hnazliayaz@gmail.com>
2021-06-09 11:27:19 +0100

    quotes

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [greeting, setGreeting] = useState();
+  const [quotes, setQuotes] = useState();
 
   useEffect(() => {
     fetch("/api")
       .then((res) => res.json())
-      .then((data) => setGreeting(data));
+      .then((data) => {
+        console.log(data);
+        setQuotes(data);
+      });
   }, []);
 
-  return <div className="App">{greeting?.message} from client</div>;
+  return (
+    <div className="App">
+      {quotes?.map((quote) => (
+        <div>
+          <h1>{quote.text}</h1>
+          <p>{quote.author}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -6,11 +6,34 @@ const app = express();
 
 app.use(cors());
 
+const quotes = [
+  {
+    text: "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    author: "Thomas Edison",
+  },
+  {
+    text: "You can observe a lot just by watching.",
+    author: "Yogi Berra",
+  },
+  {
+    text: "A house divided against itself cannot stand.",
+    author: "Abraham Lincoln",
+  },
+  {
+    text: "Difficulties increase the nearer we get to the goal.",
+    author: "Johann Wolfgang von Goethe",
+  },
+  {
+    text: "Fate is in your hands and no one elses",
+    author: "Byron Pulsifer",
+  },
+];
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
 app.get("/api", (req, res) => {
-  res.send({ message: "hello" });
+  res.send(quotes);
 });
 
 app.get("*", (req, res) => {
