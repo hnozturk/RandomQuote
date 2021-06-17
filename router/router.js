8a3b3f9a3723b09792d312f0320427857eb618ca
Nazli Ayaz <hnazliayaz@gmail.com>
2021-06-17 08:56:32 +0100

    added cursor

--- router/router.js
+++ router/router.js
@@ -2,8 +2,6 @@ const express = require("express");
 const router = express.Router();
 const Quote = require("../model/quotes");
 
-
-
 router.get("/api", async (req, res) => {
   res.send(await Quote.find());
 });
